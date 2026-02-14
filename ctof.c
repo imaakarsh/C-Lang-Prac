08500664a216f75bd5afd9f6ba84298eb3fbdd2c
imaakarsh <aakarsh027@outlook.com>
2026-02-14 23:08:40 +0530

    Add initial implementation of clac function and main program in ctof.c

---
+++ ctof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int clac(int science, int maths);
+int main(){
+    int sc = 98;
+    int maths =59;
+
+    printf("%d",clac( sc,  maths));
+    return 0;
+}
+
+int clac(int science, int maths){
+    return((science+maths)/2)*100;
+}
