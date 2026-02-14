df5b8dbb7b91ab0f696445ad31266d3b5c2b4506
imaakarsh <aakarsh027@outlook.com>
2026-02-14 21:14:03 +0530

    Refactor main function to remove sum calculation and implement multiple function for multiplication output

--- passing-argument.c
+++ passing-argument.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int sum(int a , int b);
+void multiple(int n);
 int main(){
-    int a,b;
-    printf("Enter the 1st number");
-    scanf("%d",&a);
-    printf("Enter the 2nd Number");
-    scanf("%d",&b);
-    int s = sum(a,b);
-    printf("Sum :%d",s);
+    int a,b,n;
+    printf("Enter number:");
+    scanf("%d",&n);
+    multiple(n);
     return 0;
 }
 
 int sum(int a, int b){
     return a+b;
+}
+
+void multiple(int n){
+    for ( int i=1; i<=10; i++)
+    {
+        printf("%d",i*n);
+    }
+    
 }
