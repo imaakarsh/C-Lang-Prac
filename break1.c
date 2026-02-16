b18f5d72b53b55d8d9ddba35b0d3ae5ee8ef61c3
imaakarsh <aakarsh027@outlook.com>
2026-02-17 00:55:54 +0530

    Add initial implementation of break1.c with user input handling

---
+++ break1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    do{
+        printf("ENter the number: ");
+        scanf("%d",&n);
+
+        if(n%2!=0){
+            break;
+        }
+         
+    } while(1);
+    printf("thanku");
+    return 0;
+
+    
