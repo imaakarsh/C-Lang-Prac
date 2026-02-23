1894db3fd45cf4bf68688068b9d31d4b6ded90f2
imaakarsh <aakarsh027@outlook.com>
2026-02-23 17:22:48 +0530

    Add initial implementation of reverse.c for string reversal

---
+++ .vscode/reverse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+    void main(){
+    char str[100],temp;
+    int i=0,j=0;
+    printf("Enter the String: ");
+    gets(str);
+    j=strlen(str)-1;
+    while(i<j){
+        temp = str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        i++;
+        j--;
+    }
+    printf("reverse: ");
+    puts(str);
+}
