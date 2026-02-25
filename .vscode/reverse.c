a384609f5c42dc2dde76a51863bfe1128d51c451
imaakarsh <aakarsh027@outlook.com>
2026-02-25 19:58:01 +0530

    Add digit-to-words program and update reverse implementation

--- .vscode/reverse.c
+++ .vscode/reverse.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
 #include<string.h>
-    void main(){
-    char str[100], temp;
-    int i=0,j=0;
-    printf("Enter the String : ");
-    gets(str);
-    j=strlen(str);
-    while(i<j){
-        temp = str[i];
-        str[i]=str[j];
-        str[i]=temp;
-        i++;
-        j--;
-    }
-    printf("reverse");
-    puts(str);
-    }
+int main(){
+    char str[100];
+    int i ,len;
+    scanf("%s",str);
+
+    len =strlen(str);
+      
+}
